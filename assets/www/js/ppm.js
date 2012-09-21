bbb41bd862851f65320c52568c593aed3f8900c2
colacao <caoyueqq@163.com>
2012-09-21 18:23:03 +0800

    daf

--- assets/www/js/ppm.js
+++ assets/www/js/ppm.js
@@ -1,3 +1,35 @@
+var chartdata = {"code":0,"message":"","sql":"CALL query_page_load('2012-8-21', '2012-9-20', 'DAY', null,NULL, 100101, 'NULL', 'Company')","type":"DAY","date":"2012-09-20T00:58:49.888Z","data":[{"NULL":"3138.1657","Date":1345478400000,"Company":"T"},{"NULL":"3470.7489","Date":1345478400000,"Company":"U"},{"NULL":"4213.7169","Date":1345478400000,"Company":""},{"NULL":"5530.1651","Date":1345478400000,"Company":"M"},{"NULL":"3071.1677","Date":1345564800000,"Company":"T"},{"NULL":"3472.6176","Date":1345564800000,"Company":"U"},{"NULL":"4246.7434","Date":1345564800000,"Company":""},{"NULL":"5500.5333","Date":1345564800000,"Company":"M"},{"NULL":"3870.1367","Date":1345651200000,"Company":"U"},{"NULL":"4815.4212","Date":1345651200000,"Company":""},{"NULL":"5767.8297","Date":1345651200000,"Company":"M"},{"NULL":"3340.6169","Date":1345651200000,"Company":"T"},{"NULL":"3644.4134","Date":1345737600000,"Company":"U"},{"NULL":"4264.5787","Date":1345737600000,"Company":""},{"NULL":"5647.5830","Date":1345737600000,"Company":"M"},{"NULL":"3143.3344","Date":1345737600000,"Company":"T"},{"NULL":"4187.4051","Date":1345824000000,"Company":""},{"NULL":"5672.2549","Date":1345824000000,"Company":"M"},{"NULL":"3048.2362","Date":1345824000000,"Company":"T"},{"NULL":"3440.0354","Date":1345824000000,"Company":"U"},{"NULL":"4141.5764","Date":1345910400000,"Company":""},{"NULL":"5925.9120","Date":1345910400000,"Company":"M"},{"NULL":"3095.8140","Date":1345910400000,"Company":"T"},{"NULL":"3459.9446","Date":1345910400000,"Company":"U"},{"NULL":"4408.6938","Date":1345996800000,"Company":""},{"NULL":"5591.1852","Date":1345996800000,"Company":"M"},{"NULL":"3200.7424","Date":1345996800000,"Company":"T"},{"NULL":"3611.6949","Date":1345996800000,"Company":"U"},{"NULL":"5529.4340","Date":1346083200000,"Company":"M"},{"NULL":"3216.6607","Date":1346083200000,"Company":"T"},{"NULL":"3575.3969","Date":1346083200000,"Company":"U"},{"NULL":"4421.1813","Date":1346083200000,"Company":""},{"NULL":"3200.6688","Date":1346169600000,"Company":"T"},{"NULL":"3562.7908","Date":1346169600000,"Company":"U"},{"NULL":"4316.6307","Date":1346169600000,"Company":""},{"NULL":"5617.1408","Date":1346169600000,"Company":"M"},{"NULL":"3587.8584","Date":1346256000000,"Company":"T"},{"NULL":"3723.6229","Date":1346256000000,"Company":"U"},{"NULL":"4644.8132","Date":1346256000000,"Company":""},{"NULL":"5678.4254","Date":1346256000000,"Company":"M"},{"NULL":"3674.5180","Date":1346342400000,"Company":"T"},{"NULL":"3763.3207","Date":1346342400000,"Company":"U"},{"NULL":"4632.8220","Date":1346342400000,"Company":""},{"NULL":"5840.3590","Date":1346342400000,"Company":"C"},{"NULL":"6590.9248","Date":1346342400000,"Company":"M"},{"NULL":"3105.7439","Date":1346428800000,"Company":"T"},{"NULL":"3566.6696","Date":1346428800000,"Company":"U"},{"NULL":"4328.8833","Date":1346428800000,"Company":""},{"NULL":"6003.2770","Date":1346428800000,"Company":"M"},{"NULL":"3520.0595","Date":1346515200000,"Company":"U"},{"NULL":"4193.7729","Date":1346515200000,"Company":""},{"NULL":"6230.4400","Date":1346515200000,"Company":"M"},{"NULL":"3055.3590","Date":1346515200000,"Company":"T"},{"NULL":"3876.8692","Date":1346601600000,"Company":"U"},{"NULL":"4780.0411","Date":1346601600000,"Company":""},{"NULL":"5768.0980","Date":1346601600000,"Company":"M"},{"NULL":"3959.5647","Date":1346601600000,"Company":"T"},{"NULL":"4668.8112","Date":1346688000000,"Company":""},{"NULL":"5535.0488","Date":1346688000000,"Company":"M"},{"NULL":"3854.2859","Date":1346688000000,"Company":"T"},{"NULL":"3798.3683","Date":1346688000000,"Company":"U"},{"NULL":"4398.9119","Date":1346774400000,"Company":""},{"NULL":"5401.5807","Date":1346774400000,"Company":"M"},{"NULL":"3521.5158","Date":1346774400000,"Company":"T"},{"NULL":"3575.5079","Date":1346774400000,"Company":"U"},{"NULL":"5271.0416","Date":1346860800000,"Company":"M"},{"NULL":"3150.8701","Date":1346860800000,"Company":"T"},{"NULL":"3497.5515","Date":1346860800000,"Company":"U"},{"NULL":"4258.6734","Date":1346860800000,"Company":""},{"NULL":"5739.7419","Date":1346947200000,"Company":"M"},{"NULL":"3194.8460","Date":1346947200000,"Company":"T"},{"NULL":"3660.9301","Date":1346947200000,"Company":"U"},{"NULL":"4296.4693","Date":1346947200000,"Company":""},{"NULL":"3168.0512","Date":1347033600000,"Company":"T"},{"NULL":"3670.8405","Date":1347033600000,"Company":"U"},{"NULL":"4229.5626","Date":1347033600000,"Company":""},{"NULL":"6247.8864","Date":1347033600000,"Company":"M"},{"NULL":"3129.5862","Date":1347120000000,"Company":"T"},{"NULL":"3599.1729","Date":1347120000000,"Company":"U"},{"NULL":"4204.7176","Date":1347120000000,"Company":""},{"NULL":"6257.3566","Date":1347120000000,"Company":"M"},{"NULL":"3656.2655","Date":1347206400000,"Company":"U"},{"NULL":"4381.5037","Date":1347206400000,"Company":""},{"NULL":"5891.2548","Date":1347206400000,"Company":"M"},{"NULL":"3380.4550","Date":1347206400000,"Company":"T"},{"NULL":"3606.9685","Date":1347292800000,"Company":"U"},{"NULL":"4311.9030","Date":1347292800000,"Company":""},{"NULL":"6036.7910","Date":1347292800000,"Company":"M"},{"NULL":"3386.9831","Date":1347292800000,"Company":"T"},{"NULL":"4386.9727","Date":1347379200000,"Company":""},{"NULL":"5679.2049","Date":1347379200000,"Company":"M"},{"NULL":"3358.1952","Date":1347379200000,"Company":"T"},{"NULL":"3678.0662","Date":1347379200000,"Company":"U"},{"NULL":"4228.9393","Date":1347465600000,"Company":""},{"NULL":"5665.6500","Date":1347465600000,"Company":"M"},{"NULL":"3136.4311","Date":1347465600000,"Company":"T"},{"NULL":"3538.0862","Date":1347465600000,"Company":"U"},{"NULL":"5517.0315","Date":1347552000000,"Company":"M"},{"NULL":"3035.9277","Date":1347552000000,"Company":"T"},{"NULL":"3440.8076","Date":1347552000000,"Company":"U"},{"NULL":"4107.5908","Date":1347552000000,"Company":""},{"NULL":"5906.6392","Date":1347638400000,"Company":"M"},{"NULL":"3003.8391","Date":1347638400000,"Company":"T"},{"NULL":"3415.0316","Date":1347638400000,"Company":"U"},{"NULL":"4061.5127","Date":1347638400000,"Company":""},{"NULL":"2966.8061","Date":1347724800000,"Company":"T"},{"NULL":"3461.6470","Date":1347724800000,"Company":"U"},{"NULL":"4074.4212","Date":1347724800000,"Company":""},{"NULL":"6386.5003","Date":1347724800000,"Company":"M"},{"NULL":"3065.7638","Date":1347811200000,"Company":"T"},{"NULL":"3508.6545","Date":1347811200000,"Company":"U"},{"NULL":"4191.0955","Date":1347811200000,"Company":""},{"NULL":"5665.1373","Date":1347811200000,"Company":"M"},{"NULL":"3501.5380","Date":1347897600000,"Company":"U"},{"NULL":"4094.9890","Date":1347897600000,"Company":""},{"NULL":"5106.8091","Date":1347897600000,"Company":"M"},{"NULL":"3088.9568","Date":1347897600000,"Company":"T"},{"NULL":"3458.1797","Date":1347984000000,"Company":"U"},{"NULL":"4023.9002","Date":1347984000000,"Company":""},{"NULL":"5346.9111","Date":1347984000000,"Company":"M"},{"NULL":"3142.3660","Date":1347984000000,"Company":"T"}]};
+	
+	
+	String.prototype.replaceWith = function(d) {
+	return this.replace(/\{\$(\w+)\}/g, function(a, c) {
+		if (c in d) {
+			return d[c];
+		} else {
+			return a;
+		}
+	});
+};
+Date.prototype.toDateString = function(split) {
+	split = split || "_";
+	var tempArr = [this.getFullYear(), this.getMonth() + 1, this.getDate()];
+	return tempArr.join(split);
+}
+Date.prototype.toDate2String = function(split) {
+	split = split || "_";
+	var tempArr = [this.getMonth() + 1, this.getDate().toString().pad(2)];
+	return tempArr.join(split);
+}
+Date.prototype.toDateTimeString = function(split) {
+	split = split || "_";
+	var tempArr = [this.getHours()];
+	return tempArr.join(split);
+}
+Date.prototype.toDateTime2String = function(split) {
+	split = split || "_";
+	var tempArr = [this.getFullYear(), this.getMonth() + 1, this.getDate(), this.getHours()];
+	return tempArr.join(split);
+}
 $(function() {
 	var isAndroid = (/android/gi).test(navigator.appVersion),
 		isIDevice = (/iphone|ipad/gi).test(navigator.appVersion),
@@ -138,9 +170,176 @@ $(function(){
 		$('#scroller')[0].style.marginLeft =  "0px";
 		$('#bb1').hide();
 	}
-	function abc(){
-		var data = {"code":0,"message":"","sql":"CALL query_page_load('2012-8-21', '2012-9-20', 'DAY', null,NULL, 100101, 'NULL', 'Company')","type":"DAY","date":"2012-09-20T00:58:49.888Z","data":[{"NULL":"3138.1657","Date":1345478400000,"Company":"T"},{"NULL":"3470.7489","Date":1345478400000,"Company":"U"},{"NULL":"4213.7169","Date":1345478400000,"Company":""},{"NULL":"5530.1651","Date":1345478400000,"Company":"M"},{"NULL":"3071.1677","Date":1345564800000,"Company":"T"},{"NULL":"3472.6176","Date":1345564800000,"Company":"U"},{"NULL":"4246.7434","Date":1345564800000,"Company":""},{"NULL":"5500.5333","Date":1345564800000,"Company":"M"},{"NULL":"3870.1367","Date":1345651200000,"Company":"U"},{"NULL":"4815.4212","Date":1345651200000,"Company":""},{"NULL":"5767.8297","Date":1345651200000,"Company":"M"},{"NULL":"3340.6169","Date":1345651200000,"Company":"T"},{"NULL":"3644.4134","Date":1345737600000,"Company":"U"},{"NULL":"4264.5787","Date":1345737600000,"Company":""},{"NULL":"5647.5830","Date":1345737600000,"Company":"M"},{"NULL":"3143.3344","Date":1345737600000,"Company":"T"},{"NULL":"4187.4051","Date":1345824000000,"Company":""},{"NULL":"5672.2549","Date":1345824000000,"Company":"M"},{"NULL":"3048.2362","Date":1345824000000,"Company":"T"},{"NULL":"3440.0354","Date":1345824000000,"Company":"U"},{"NULL":"4141.5764","Date":1345910400000,"Company":""},{"NULL":"5925.9120","Date":1345910400000,"Company":"M"},{"NULL":"3095.8140","Date":1345910400000,"Company":"T"},{"NULL":"3459.9446","Date":1345910400000,"Company":"U"},{"NULL":"4408.6938","Date":1345996800000,"Company":""},{"NULL":"5591.1852","Date":1345996800000,"Company":"M"},{"NULL":"3200.7424","Date":1345996800000,"Company":"T"},{"NULL":"3611.6949","Date":1345996800000,"Company":"U"},{"NULL":"5529.4340","Date":1346083200000,"Company":"M"},{"NULL":"3216.6607","Date":1346083200000,"Company":"T"},{"NULL":"3575.3969","Date":1346083200000,"Company":"U"},{"NULL":"4421.1813","Date":1346083200000,"Company":""},{"NULL":"3200.6688","Date":1346169600000,"Company":"T"},{"NULL":"3562.7908","Date":1346169600000,"Company":"U"},{"NULL":"4316.6307","Date":1346169600000,"Company":""},{"NULL":"5617.1408","Date":1346169600000,"Company":"M"},{"NULL":"3587.8584","Date":1346256000000,"Company":"T"},{"NULL":"3723.6229","Date":1346256000000,"Company":"U"},{"NULL":"4644.8132","Date":1346256000000,"Company":""},{"NULL":"5678.4254","Date":1346256000000,"Company":"M"},{"NULL":"3674.5180","Date":1346342400000,"Company":"T"},{"NULL":"3763.3207","Date":1346342400000,"Company":"U"},{"NULL":"4632.8220","Date":1346342400000,"Company":""},{"NULL":"5840.3590","Date":1346342400000,"Company":"C"},{"NULL":"6590.9248","Date":1346342400000,"Company":"M"},{"NULL":"3105.7439","Date":1346428800000,"Company":"T"},{"NULL":"3566.6696","Date":1346428800000,"Company":"U"},{"NULL":"4328.8833","Date":1346428800000,"Company":""},{"NULL":"6003.2770","Date":1346428800000,"Company":"M"},{"NULL":"3520.0595","Date":1346515200000,"Company":"U"},{"NULL":"4193.7729","Date":1346515200000,"Company":""},{"NULL":"6230.4400","Date":1346515200000,"Company":"M"},{"NULL":"3055.3590","Date":1346515200000,"Company":"T"},{"NULL":"3876.8692","Date":1346601600000,"Company":"U"},{"NULL":"4780.0411","Date":1346601600000,"Company":""},{"NULL":"5768.0980","Date":1346601600000,"Company":"M"},{"NULL":"3959.5647","Date":1346601600000,"Company":"T"},{"NULL":"4668.8112","Date":1346688000000,"Company":""},{"NULL":"5535.0488","Date":1346688000000,"Company":"M"},{"NULL":"3854.2859","Date":1346688000000,"Company":"T"},{"NULL":"3798.3683","Date":1346688000000,"Company":"U"},{"NULL":"4398.9119","Date":1346774400000,"Company":""},{"NULL":"5401.5807","Date":1346774400000,"Company":"M"},{"NULL":"3521.5158","Date":1346774400000,"Company":"T"},{"NULL":"3575.5079","Date":1346774400000,"Company":"U"},{"NULL":"5271.0416","Date":1346860800000,"Company":"M"},{"NULL":"3150.8701","Date":1346860800000,"Company":"T"},{"NULL":"3497.5515","Date":1346860800000,"Company":"U"},{"NULL":"4258.6734","Date":1346860800000,"Company":""},{"NULL":"5739.7419","Date":1346947200000,"Company":"M"},{"NULL":"3194.8460","Date":1346947200000,"Company":"T"},{"NULL":"3660.9301","Date":1346947200000,"Company":"U"},{"NULL":"4296.4693","Date":1346947200000,"Company":""},{"NULL":"3168.0512","Date":1347033600000,"Company":"T"},{"NULL":"3670.8405","Date":1347033600000,"Company":"U"},{"NULL":"4229.5626","Date":1347033600000,"Company":""},{"NULL":"6247.8864","Date":1347033600000,"Company":"M"},{"NULL":"3129.5862","Date":1347120000000,"Company":"T"},{"NULL":"3599.1729","Date":1347120000000,"Company":"U"},{"NULL":"4204.7176","Date":1347120000000,"Company":""},{"NULL":"6257.3566","Date":1347120000000,"Company":"M"},{"NULL":"3656.2655","Date":1347206400000,"Company":"U"},{"NULL":"4381.5037","Date":1347206400000,"Company":""},{"NULL":"5891.2548","Date":1347206400000,"Company":"M"},{"NULL":"3380.4550","Date":1347206400000,"Company":"T"},{"NULL":"3606.9685","Date":1347292800000,"Company":"U"},{"NULL":"4311.9030","Date":1347292800000,"Company":""},{"NULL":"6036.7910","Date":1347292800000,"Company":"M"},{"NULL":"3386.9831","Date":1347292800000,"Company":"T"},{"NULL":"4386.9727","Date":1347379200000,"Company":""},{"NULL":"5679.2049","Date":1347379200000,"Company":"M"},{"NULL":"3358.1952","Date":1347379200000,"Company":"T"},{"NULL":"3678.0662","Date":1347379200000,"Company":"U"},{"NULL":"4228.9393","Date":1347465600000,"Company":""},{"NULL":"5665.6500","Date":1347465600000,"Company":"M"},{"NULL":"3136.4311","Date":1347465600000,"Company":"T"},{"NULL":"3538.0862","Date":1347465600000,"Company":"U"},{"NULL":"5517.0315","Date":1347552000000,"Company":"M"},{"NULL":"3035.9277","Date":1347552000000,"Company":"T"},{"NULL":"3440.8076","Date":1347552000000,"Company":"U"},{"NULL":"4107.5908","Date":1347552000000,"Company":""},{"NULL":"5906.6392","Date":1347638400000,"Company":"M"},{"NULL":"3003.8391","Date":1347638400000,"Company":"T"},{"NULL":"3415.0316","Date":1347638400000,"Company":"U"},{"NULL":"4061.5127","Date":1347638400000,"Company":""},{"NULL":"2966.8061","Date":1347724800000,"Company":"T"},{"NULL":"3461.6470","Date":1347724800000,"Company":"U"},{"NULL":"4074.4212","Date":1347724800000,"Company":""},{"NULL":"6386.5003","Date":1347724800000,"Company":"M"},{"NULL":"3065.7638","Date":1347811200000,"Company":"T"},{"NULL":"3508.6545","Date":1347811200000,"Company":"U"},{"NULL":"4191.0955","Date":1347811200000,"Company":""},{"NULL":"5665.1373","Date":1347811200000,"Company":"M"},{"NULL":"3501.5380","Date":1347897600000,"Company":"U"},{"NULL":"4094.9890","Date":1347897600000,"Company":""},{"NULL":"5106.8091","Date":1347897600000,"Company":"M"},{"NULL":"3088.9568","Date":1347897600000,"Company":"T"},{"NULL":"3458.1797","Date":1347984000000,"Company":"U"},{"NULL":"4023.9002","Date":1347984000000,"Company":""},{"NULL":"5346.9111","Date":1347984000000,"Company":"M"},{"NULL":"3142.3660","Date":1347984000000,"Company":"T"}]};
+	var NOW = new Date();
+var xData = {}
+function getArr(data,key){
+	var comps = {};
+	$("#chkComp input").each(function(i,item){
+		if(item.checked){
+			comps[item.value] = item.value;
+		}
+	});
+	
 		
+
+	var arr = [];
+	for(var i = 0;i<data.length;i++){
+		var ttype = data[i].Company ? {
+			"其它": "其它",
+			"U": "联通",
+			"T": "电信",
+			"M": "移动"
+		}[data[i].Company] : "其它";
+		if(ttype == key && (ttype in comps)){
+		
+			var tempDateString  = new Date(data[i].Date).toDate2String('/');
+			if(!xData[tempDateString]){
+				xData[tempDateString] = tempDateString
+			};
+			arr.push(parseInt(data[i][LAST_TAB]*1));
+		}
+		
+	}
+
+	
+	return arr;
+}
+
+function abc(type) {
+	$('#one').html("");
+	var el = $('#selectmenu24');
+	var eldata = [el.data('begin'),el.data('end')];
+	var begin = eldata[0];
+	var end = eldata[1];
+	var pageid = $('#searchinput1').val().split(' ')[1];
+	var params = {
+
+		type:$('#selectmenu2').val(),
+		begin: begin ? begin : new Date(new Date().setDate(-30)).toDateString("-"),
+		end:end ?end: new Date().toDateString("-"),
+		datetype: "DAY",
+		pageid:pageid ? pageid : "100101",
+		city: null
+	};
+	// $('#one').html('<img src="images/demo.png"  style="width:100%;height:100%" />');
+	// return ;
+	LAST_TAB = params.type;
+	
+	// var url = "./data/{$begin}/{$end}/{$datetype}/{$city}/NULL/{$pageid}/{$type}/Company".replaceWith(params);
+	// $.ajax({
+		// url: url,
+		// dataType: "json",
+		// success: function(data) {
+			xData = {};
+			var data = chartdata;
+			
+			NOW = data.date;
+			if (!data.data.length) {
+				$('#one').html("<div style='width:100%;height:100%;min-height: 400px;background-color:#fff;background-image:url(images/sorry.png);text-align:center;background-repeat:no-repeat;background-position: center;'></div>");
+				return;
+			}
+			var seriesOptions  = [];
+			var  names = {};
+			var seriesCounter = 0;
+			
+			for(var i=0;i<data.data.length;i++){
+				var ttype = data.data[i].Company ? {
+						"其它": "其它",
+						"U": "联通",
+						"T": "电信",
+						"M": "移动"
+				}[data.data[i].Company] : "其它";
+				
+				if(!names[ttype]){
+					names[ttype] = ttype;
+				}
+				
+			}
+			
+			for(var b in names){
+				seriesCounter++;
+					//debugger;
+				var arr = getArr(data.data,b);
+				//console.log(arr);
+				if(arr.length){
+					seriesOptions[seriesCounter] = {
+						name: b,
+						data: arr,
+						visible :arr.length?true:false,
+						marker : {
+							enabled : true,
+							radius : 3
+						}
+					}
+				}
+			}
+			var _seriesOptions=[];
+			for(var i=0;i<seriesOptions.length;i++){
+				if(seriesOptions[i]){
+					_seriesOptions.push(seriesOptions[i]);
+				}
+			}
+			
+			Highcharts.setOptions({
+				global: {
+					useUTC: false
+				}
+			});
+			var xAc = [];
+			for(var b in xData){
+				xAc.push(b);
+			}
+			xAc.sort();
+		
+			chart = new Highcharts.Chart({
+				chart: {
+					type: 'spline',
+					renderTo: 'one'
+				},
+				 title: {
+					text: $('#selectmenu2').val().split(' ')[1] ,
+					style :{
+						color: '#6D869F',
+						fontSize: '13.5px',
+						fontWeight:"bold" 
+					}
+				},
+				subtitle: {
+					text: ' '
+				},
+				// xAxis: {
+					// categories: xAc
+				// },
+				yAxis:{
+					 title: {
+						text: '单位 (ms)'
+					},
+					plotLines: [{
+						value: 0,
+						width: 1,
+						color: '#808080'
+					}]
+				},
+				plotOptions: {
+					series: {
+						compare: 'percent'
+					}
+				},
+				tooltip: {
+					crosshairs: true,
+					shared: true
+				},
+				series: _seriesOptions
+			});
+		//	console.log(_seriesOptions);
+			$('#scroller')[0].style.marginLeft = "-"+$(window).width()+ "px";
+			$('#bb1').show();
+	
+		// }
+	// });
+}
+	
+	
 		// $('#scroller')[0].style.marginLeft = "-"+$(window).width()+ "px";
 		// $('#bb1').show();
-	}
+	
